3efdd3dc9657efc50bc22219857e010ef9e69d79
VicWrest <cyganov0206@gmail.com>
2024-10-22 04:23:22 +1000

    finish

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -11,16 +11,6 @@ export const registration = async (userName) => {
         alert(e?.response?.statusText);
     }
 };
-
-export const start = async () => {
-    try{
-        const {data} = await $authHost.get('/')
-        return data;
-    }
-    catch(e){
-        console.log(e);
-    }
-};
 //для моего проекта не нужна данная функция тк авторизация происходит автоматически,
 //при запуске приложения
 export const check = async () => {
