f05e51d0c551d1640694489822cfb0bf278d3073
VicWrest <cyganov0206@gmail.com>
2024-10-23 06:01:26 +1000

    with updated btns

---
+++ src/components/Desktop-menu/Desktop-menu.js
@@ -0,0 +1,36 @@
+import React, { useContext, useRef, useState } from 'react';
+import "./Mobile-menu.css"
+import menuButton from '../../assets/menu-button.png';
+import { ADMIN_ROUTE, BASKET_ROUTE, REWIEW_ROUTE } from '../../utils/consts';
+import { useNavigate } from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+import {Context} from "../../index";
+import { useClickOutside } from '../../hooks/useClickOutside';
+
+const DesktopMenu = observer((props) => {
+    const {user} = useContext(Context)
+    const history = useNavigate()
+    const [isOpen, setOpen] = useState();
+    const menuRef = useRef(null);
+    useClickOutside(menuRef, () => {
+        if(isOpen){
+            setTimeout(() => setOpen(false), 50)
+        }
+    })
+    return (
+        <div className='navbar-buttons'>
+        <div className='navbar-btns'> 
+        <div className='client-navbar-btns'>
+            <button className='review-navbar-btn' onClick={()=>history(REWIEW_ROUTE)}>
+                Отзывы
+            </button>
+            <button className='basket-navbar-btn' onClick={()=>history(BASKET_ROUTE)}>
+                Корзина
+            </button>
+        </div>
+    </div>
+    </div>
+    );
+});
+
+export default DesktopMenu;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import { observer } from 'mobx-react-lite';
 import './NavBar.css'
 import MobileMenu from '../Mobile-menu/Mobile-menu';
+import DesktopMenu from '../Desktop-menu/Desktop-menu';
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
@@ -14,26 +15,14 @@ const NavBar = observer(() => {
             <div className='home-work-string' onClick={()=>history(SHOP_ROUTE)} >
             Домашняя мастерская десертов
             </div>
-            <div className='navbar-buttons'>
-            <div className='navbar-btns'> 
-                <div className='client-navbar-btns'>
-                {user.isAdmin && <button 
-                        className='admin-navbar-btn'
-                        onClick={() => history(ADMIN_ROUTE)}>
-                            Админ панель
-                        </button>}
-            <button className='review-navbar-btn' onClick={()=>history(REWIEW_ROUTE)}>
-                    Отзывы
-            </button>
-            <button className='basket-navbar-btn' onClick={()=>history(BASKET_ROUTE)}>
-            Корзина
-            </button>
-            </div>
+            {user.isAdmin? 
+                <DesktopMenu />
+                :
                 <MobileMenu  />
+            }
             </div>
-            </div>
-        </div>)
-        });
+    )
+});
 
         
  
