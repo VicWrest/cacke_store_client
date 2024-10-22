36d06520b9b4b36db4907b9e8e58e4ff18293d6c
VicWrest <cyganov0206@gmail.com>
2024-10-23 06:10:25 +1000

    with updated btns2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,6 @@ import {useNavigate} from "react-router-dom";
 import { observer } from 'mobx-react-lite';
 import './NavBar.css'
 import MobileMenu from '../Mobile-menu/Mobile-menu';
-import DesktopMenu from '../Desktop-menu/Desktop-menu';
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
@@ -15,14 +14,26 @@ const NavBar = observer(() => {
             <div className='home-work-string' onClick={()=>history(SHOP_ROUTE)} >
             Домашняя мастерская десертов
             </div>
-            {user.isAdmin? 
-                <DesktopMenu />
-                :
+            <div className='navbar-buttons'>
+            <div className='navbar-btns'> 
+                <div className='client-navbar-btns'>
+                {user.isAdmin && <button 
+                        className='admin-navbar-btn'
+                        onClick={() => history(ADMIN_ROUTE)}>
+                            Админ панель
+                        </button>}
+            <button className='review-navbar-btn' onClick={()=>history(REWIEW_ROUTE)}>
+                    Отзывы
+            </button>
+            <button className='basket-navbar-btn' onClick={()=>history(BASKET_ROUTE)}>
+            Корзина
+            </button>
+                </div>
                 <MobileMenu  />
-            }
             </div>
-    )
-});
+            </div>
+        </div>)
+        });
 
         
  
