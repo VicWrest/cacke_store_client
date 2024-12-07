e48bf00895ceb3f5f1525381903454d09d8ef535
VicWrest <cyganov0206@gmail.com>
2024-12-07 21:07:16 +1000

    test for new users8

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -8,7 +8,8 @@ export const registration = async (userName) => {
         return jwtDecode(data.token);
     }
     catch(e){
-        alert(e?.response?.statusText);
+        alert(`ERROR BY Registration`)
+        // alert(e?.response?.statusText);
     }
 };
 //для моего проекта не нужна данная функция тк авторизация происходит автоматически,
